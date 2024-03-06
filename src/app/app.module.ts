bd4d01b1adc2b303727ba5d3253bebc673f044ae
fateehs <benzer1010@gmail.com>
2024-03-06 19:39:10 +0300

    desing: feature add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { ProfilePicturesComponent } from './components/user-details/profile-picture/profile-pictures/profile-pictures.component';
+import { SteamIdComponent } from './components/user-details/steam-id/steam-id.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
     UserDetailsComponent,
-    ProfilePicturesComponent
+    ProfilePicturesComponent,
+    SteamIdComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/user-details/steam-id/steam-id.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-steam-id',
+  templateUrl: './steam-id.component.html',
+  styleUrl: './steam-id.component.css'
+})
+export class SteamIdComponent {
+
+}
