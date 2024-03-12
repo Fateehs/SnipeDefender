1d7d7f52a4eacac17de67c1e6ead9ac3fe8f2e26
fateehs <benzer1010@gmail.com>
2024-03-12 19:27:06 +0300

    devs: backend data add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 const routes: Routes = [
-  { path: 'userdetails', component: UserDetailsComponent },
+  { path: '', component: HomepageComponent },
+  { path: 'userdetails', component: UserDetailsComponent }
 ];
 
 @NgModule({
--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrl: './homepage.component.css',
 })
 export class HomepageComponent {
-
+  backendVerileri = [
+    'An item',
+    'A second item',
+    'A third item',
+    'A fourth item',
+    'And a fifth one',
+  ];
 }
